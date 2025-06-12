c54fc800ce0687f3f89906f0694f6e56a69a7954
Ramon Saviato <ramonsaviato@hotmail.com>
2025-06-12 16:53:02 -0300

    add Client entity

---
+++ src/main/java/com/crud/clients/domain/entities/Client.java
@@ -0,0 +1,39 @@
+package com.crud.clients.domain.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@Table(name = "TBL_CLIENT")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+    private Double income;
+    private LocalDate birthDate;
+    private Integer children;
+
+}
