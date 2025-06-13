63e0ecba365accc95913c4d105ecadc6a017d271
Ramon Saviato <ramonsaviato@hotmail.com>
2025-06-13 13:54:43 -0300

    tweak: change controller to return ResponseEntity instead of plain dtos

--- src/main/java/com/crud/clients/controllers/ClientController.java
+++ src/main/java/com/crud/clients/controllers/ClientController.java
@@ -1,10 +1,12 @@
 package com.crud.clients.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,48 +14,53 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.crud.clients.domain.dto.ClientDTO;
 import com.crud.clients.services.ClientService;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
-
-
 @RestController
-@RequestMapping(path = "/clients",
-                produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(path = "/clients", produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ClientController {
 
     private final ClientService clientService;
 
     @GetMapping("/{id}")
-    public ClientDTO findById(@PathVariable Long id) {
-        return clientService.findById(id);
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(clientService.findById(id));
     }
 
     @GetMapping
-    public Page<ClientDTO> findAll(Pageable pageable) {
-        return clientService.findAll(pageable);
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+        return ResponseEntity.ok(clientService.findAll(pageable));
     }
 
     @PostMapping
-    public ClientDTO save(@RequestBody ClientDTO dto) {
-        return clientService.save(dto);
+    public ResponseEntity<ClientDTO> save(
+            @RequestBody ClientDTO dto,
+            UriComponentsBuilder builder,
+            HttpServletRequest request) {
+        ClientDTO saved = clientService.save(dto);
+        URI uri = builder.path("{path}/{id}")
+                .buildAndExpand(request.getRequestURI(), saved.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(saved);
     }
 
     @PutMapping("/{id}")
-    public ClientDTO update(@PathVariable Long id, @RequestBody ClientDTO dto) {
-        return clientService.update(id, dto);
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto) {
+        return ResponseEntity.ok(clientService.update(id, dto));
     }
-    
+
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteById(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
         clientService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
